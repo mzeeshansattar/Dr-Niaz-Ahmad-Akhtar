9ed48c7d051152bd1e837e0d85ad00aaf86bff47
mzeeshansattar <152576866+mzeeshansattar@users.noreply.github.com>
2024-04-06 09:29:36 +0500

    uploaded

---
+++ one.js
@@ -0,0 +1,28 @@
+document.getElementById("earlylife").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("EarlyLife.html"), "_blank";
+});
+document.getElementById("career").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("Career.html", "_blank");
+});
+document.getElementById("research").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("Research.html", "_blank");
+});
+document.getElementById("awards").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("Awards.html", "_blank");
+});
+document.getElementById("books").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("Books.html", "_blank");
+});
+document.getElementById("books").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.open("Books.html", "_blank");
+});
+document.getElementById("h1").addEventListener("click", function () {
+  // Redirect to EarlyLife.html
+  window.location.href = "one.html";
+});
